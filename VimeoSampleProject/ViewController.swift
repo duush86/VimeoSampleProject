ef1dd4c153b5a740c3f0d6c16dcd602d8e7e5cf2
Antonio <j.antonio.oro@gmail.com>
2020-02-07 12:23:02 -0600

    Initial commit

--- VimeoSampleProject/ViewController.swift
+++ VimeoSampleProject/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import PlayerKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    private struct Constants {
+        static let VideoURL = URL(string: "https://player.vimeo.com/external/389308711.sd.mp4?s=01f1a97e6a0f6d1c3fb2bf7fe8e3d770567227b1&profile_id=164")!
+    }
+        
 
+    private var player = RegularPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        player.view.frame = self.view.bounds
+
+        self.view.insertSubview(player.view, at: 0)
+        
+        player.set(AVURLAsset(url: Constants.VideoURL))
+
+        player.play()
+        
     }
 
 
